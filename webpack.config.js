a9e5c90cab959628e47d5c0c1d81021230920b46
Jye-Ming Serres <jyeming1001@gmail.com>
2025-11-04 13:43:37 -0500

    chore: multiple webpack entry points and start of development

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,34 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/script.js"),
+  entry:  {
+    index: path.resolve(__dirname, "src/assets/js/index.js"),
+    projects: path.resolve(__dirname, "src/assets/js/projects.js"),
+    resume: path.resolve(__dirname, "src/assets/js/resume.js"),
+  }, 
   output: {
-    path: path.resolve(__dirname, "docs"), // destination
-    filename: "bundle.js",
-    clean: { // destination folder is emptied every time
-        keep: /CNAME/, // keep CNAME
+    path: path.resolve(__dirname, "docs"),
+    filename: "[name]_bundle.js",
+    clean: {
+      keep: /CNAME/,
     },
   },
   plugins: [
-    // copies HTML files in the destination
+    // copies HTML files in the destination, see https://github.com/jantimon/html-webpack-plugin
     new HTMLWebpackPlugin({
+      filename: "index.html",
       template: path.resolve(__dirname, "src/index.html"),
+      chunks: ["index"],
+    }),
+    new HTMLWebpackPlugin({
+      filename: "projects.html",
+      template: path.resolve(__dirname, "src/projects.html"),
+      chunks: ["projects"],
+    }),
+    new HTMLWebpackPlugin({
+      filename: "resume.html",
+      template: path.resolve(__dirname, "src/resume.html"),
+      chunks: ["resume"],
     }),
   ],
 };
