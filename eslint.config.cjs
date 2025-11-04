97427e37fcd03e53dd35a835f9e59d3ade46ab5f
Jye-Ming Serres <jyeming1001@gmail.com>
2025-11-04 10:47:44 -0500

    chore: eslint configuration

---
+++ eslint.config.cjs
@@ -0,0 +1,36 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const { defineConfig } = require("eslint/config");
+
+module.exports = defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        rules: {
+            eqeqeq: "error",
+            "max-lines": [
+                "warn",
+                { max: 200, skipBlankLines: true, skipComments: true },
+            ],
+            "no-unused-vars": "error",
+            "no-var": "error",
+            "no-console": "warn",
+            "prefer-const": "error",
+            "no-magic-numbers": [
+                "error",
+                {
+                    ignore: [-1, 0, 1, 2, 3, 4, 5],
+                    ignoreArrayIndexes: true,
+                    detectObjects: true,
+                },
+            ],
+        },
+    },
+	{
+        ignores: ["node_modules", "eslint.config.cjs", "webpack.config.js", "docs"],
+    },
+]);
