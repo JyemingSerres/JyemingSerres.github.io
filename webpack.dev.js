b2bfd854b338052b5bd15d6412ae275c69697b76
Jye-Ming Serres <jyeming1001@gmail.com>
2025-11-05 15:15:44 -0500

    chore: added css-loader and use of style-loader for devserver

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,14 +2,24 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+const destinationName = "docsdev";
+
 module.exports = merge(common, {
   mode: "development",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, destinationName),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
   },
   devtool: "inline-source-map",
   devServer: {
-    static: "./public",
+    static: destinationName,
   },
 });
