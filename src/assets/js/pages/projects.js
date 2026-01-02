a64a45b42d0976d76ac1d0ad8db3d75e5b1414e7
Jye-Ming Serres <jyeming1001@gmail.com>
2026-01-02 03:30:39 -0500

    feat: card layout and style

--- src/assets/js/pages/projects.js
+++ src/assets/js/pages/projects.js
@@ -9,7 +9,7 @@ function importAll(r) {
 }
 
 // imports all images at build time, see https://webpack.js.org/guides/dependency-management/#context-module-api
-importAll(require.context("../../data/projects/images/", false, /\.png$/, 'lazy'));
+importAll(require.context("../../data/projects/images/", false, /\.png$/));
 
 
 export default function loadProjects(parent) {
@@ -17,17 +17,46 @@ export default function loadProjects(parent) {
     wrapper.classList.add("card-wrapper");
 
     for (const project of projects) {
-        console.log(project);
-        const proj = document.createElement("div");
-        proj.classList.add("card");
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const container = document.createElement("div");
+        container.classList.add("card-img-container");
 
         const img = document.createElement("img");
-        cache["./" + project.imageFileName].then((link) => {
-            img.src = link;
-        });
-        proj.appendChild(img);
+        img.src = cache["./" + project.imageFileName];
+        img.loading = "lazy";
+        container.appendChild(img);
 
-        wrapper.appendChild(proj);
+        card.appendChild(container);
+
+        const info = document.createElement("div");
+        info.classList.add("card-info");
+
+        const infoTitle = document.createElement("p");
+        infoTitle.classList.add("card-info-title");
+        infoTitle.textContent = project.projectName;
+        info.appendChild(infoTitle);
+
+        const infoDescription = document.createElement("p");
+        infoDescription.classList.add("card-info-desc");
+        infoDescription.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+        info.appendChild(infoDescription);
+
+        const infoStack = document.createElement("div");
+        infoStack.classList.add("card-info-stack");
+
+        for (const tech of project.stack) {
+            const pill = document.createElement("div");
+            pill.textContent = tech;
+            infoStack.appendChild(pill);
+        }
+
+        info.appendChild(infoStack);
+
+        card.appendChild(info);
+
+        wrapper.appendChild(card);
     }
 
     parent.appendChild(wrapper);
